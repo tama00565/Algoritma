21f2739f7bdd1f650d8c59de659050034dd59c74
tama00565 <kakatama0606@gmail.com>
2024-01-31 13:09:21 +0700

    Algoritma

---
+++ index.js
@@ -0,0 +1,62 @@
+//  1. Mencari Keliling Persegi Panjang
+// Memasukan nilai panjang, lebar
+// Menghitung rumus: 2*P +*L
+// Menampilkan hasil
+
+// Memasukan nilai panjang, lebar
+// let panjang = 150;
+// let lebar = 40;
+
+// Menghitung Rumus 
+// let hasil = 2 * panjang + 2 * lebar;
+
+// Menampilkan hasil
+// console.log("Keliling persegi panjang adalah: " + hasil)
+
+// 2. Luas Lingkaran 
+
+// Tentukan phi dan r
+// let phi = 3.14;
+// let r = 10;
+
+// let hasill = phi * r * r;
+
+// Menampilkan hasil
+// console.log("Hasil nya: " + hasill)
+
+// let lp = 64;
+
+// let sisi  = Math.sqrt(lp);
+
+// let hasilnya = 4 * sisi;
+
+// console.log (hasilnya)
+
+// Syarat umur membuat KTP
+// Input
+// let umur = 15;
+
+// // proses/ logika
+// if(umur >= 17){
+//     result = "Membuat KTP";
+// } else {
+//     result = "Belum bisa membuat KTP"
+// }
+
+// hasil / output
+// console.log(result)
+
+// Input nilai 
+// let PABP
+// let MTK
+// let PDK
+
+// -------------
+// for(let g = 1; g <= 20; g++) {
+//     if( g % 2 == 0){
+//         maka = "genap"
+//     }else {
+//         maka = "ganjil"
+//     }
+//     console.log(g + maka);
+// }
